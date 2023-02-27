2355bad4974865f15b08a72fb4c267cf00bd68c4
obrookes <dl18206@bristol.ac.uk>
2023-02-27 14:47:43 +0000

    only take subset of all data

--- generate_kinetics_annotations.py
+++ generate_kinetics_annotations.py
@@ -7,22 +7,25 @@ from glob import glob
 def argparser():
     parser = argparse.ArgumentParser()
     parser.add_argument("--path_to_videos", type=str)
+    parser.add_argument("--data_fraction", type=float)
     parser.add_argument("--outfile", type=str)
     args = parser.parse_args()
     return args
 
-def generate_kinetics_annotation_file(path_to_videos, outfile):
+def generate_kinetics_annotation_file(path_to_videos, data_fraction, outfile):
     videos = glob(f'{path_to_videos}/**/*.mp4', recursive=True)
     df = pd.DataFrame(videos, columns=['videos'])
     df['label_index'] = 9 # Dummy label for SS training
+    index = round(len(df) * data_fraction)
+    df = df.sample(index)
     df.to_csv(outfile, sep=' ', header=None, index=False)
 
-    
 def main():
 
     args = argparser()
     generate_kinetics_annotation_file(
         path_to_videos=args.path_to_videos, 
+        data_fraction=args.data_fraction,
         outfile=args.outfile
     )
     
