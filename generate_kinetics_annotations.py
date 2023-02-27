a90b0efc58e08e575bb230aaa882710affe0c8b3
obrookes <dl18206@bristol.ac.uk>
2023-02-27 14:16:02 +0000

    small script to generate kinetics-style annotations

---
+++ generate_kinetics_annotations.py
@@ -0,0 +1,30 @@
+import argparse
+import pandas as pd
+from glob import glob
+
+
+
+def argparser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--path_to_videos", type=str)
+    parser.add_argument("--outfile", type=str)
+    args = parser.parse_args()
+    return args
+
+def generate_kinetics_annotation_file(path_to_videos, outfile):
+    videos = glob(f'{path_to_videos}/**/*.mp4', recursive=True)
+    df = pd.DataFrame(videos, columns=['videos'])
+    df['label_index'] = 9 # Dummy label for SS training
+    df.to_csv(outfile, sep=' ', header=None, index=False)
+
+    
+def main():
+
+    args = argparser()
+    generate_kinetics_annotation_file(
+        path_to_videos=args.path_to_videos, 
+        outfile=args.outfile
+    )
+    
+if __name__ == "__main__":
+    main()
